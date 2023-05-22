e8ded8edc797276c7db721afc96baa3815443ee9
BPoder <bertpoder@gmail.com>
2023-05-22 16:57:40 +0300

    ISA-BOOLEAN WIP day3 van tabel view + dropdown function

--- src/main/java/com/example/vans_back/business/van/VansService.java
+++ src/main/java/com/example/vans_back/business/van/VansService.java
@@ -1,11 +1,17 @@
 package com.example.vans_back.business.van;
 
 import com.example.vans_back.business.van.dto.VanBasicInfo;
+import com.example.vans_back.domain.van.Van;
+import com.example.vans_back.domain.van.VanAllInfo;
+import com.example.vans_back.domain.van.VanMapper;
+import com.example.vans_back.domain.van.VanService;
 import com.example.vans_back.domain.van.driver.Driver;
 import com.example.vans_back.domain.van.driver.DriverMapper;
 import com.example.vans_back.domain.van.driver.DriverService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VansService {
@@ -16,9 +22,21 @@ public class VansService {
     @Resource
     private DriverMapper driverMapper;
 
+    @Resource
+    private VanService vanService;
+
+    @Resource
+    private VanMapper vanMapper;
+
     public VanBasicInfo getUserVanInfo(Integer driverId) {
         Driver driver = driverService.getDriverBy(driverId);
         VanBasicInfo vanBasicInfo = driverMapper.toVanBasicInfo(driver);
         return vanBasicInfo;
+    }
+
+    public List<VanAllInfo> getVans(Integer cityId, Integer vanId) {
+        List<Van> allVans = vanService.getVansBy(cityId, vanId);
+        List<VanAllInfo> vans = vanMapper.toVanAllInfos(allVans);
+        return vans;
     }
 }
--- src/main/java/com/example/vans_back/domain/van/VanService.java
+++ src/main/java/com/example/vans_back/domain/van/VanService.java
@@ -1,7 +1,18 @@
 package com.example.vans_back.domain.van;
 
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VanService {
+
+    @Resource
+    private VanRepository vanRepository;
+
+    public List<Van> getVansBy(Integer cityId, Integer vanId) {
+        List<Van> vans = vanRepository.findVansBy(cityId, vanId);
+        return vans;
+    }
 }
