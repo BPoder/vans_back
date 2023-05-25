39c734156ec79be3bf77124bc361092dcc47e9d3
BPoder <bertpoder@gmail.com>
2023-05-25 14:00:56 +0300

    ISA-BOOLEAN WIP day6 get Cities dropdown

---
+++ src/main/java/com/example/vans_back/domain/van/city/CityMapper.java
@@ -0,0 +1,16 @@
+package com.example.vans_back.domain.van.city;
+
+import com.example.vans_back.business.city.dto.CityDto;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CityMapper {
+
+    @Mapping(source = "id", target = "cityId")
+    @Mapping(source = "name", target = "cityName")
+    CityDto toCityDto(City city);
+
+    List<CityDto> toCityDtos(List<City> cities);
+}
