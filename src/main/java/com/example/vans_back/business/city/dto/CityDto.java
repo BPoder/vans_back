0e1bfe6ead6626943d7eede3d9d9f63bf2d53210
Robert <test@test.ee>
2023-05-25 14:01:14 +0300

    Get cities /for dropdown

---
+++ src/main/java/com/example/vans_back/business/city/dto/CityDto.java
@@ -0,0 +1,21 @@
+package com.example.vans_back.business.city.dto;
+
+import com.example.vans_back.domain.van.city.City;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link City}
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityDto implements Serializable {
+    private Integer cityId;
+    private String cityName;
+}
