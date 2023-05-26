db5dba46d4e880942907e2404a166e5e81ccc47b
BPoder <bertpoder@gmail.com>
2023-05-26 14:32:19 +0300

    ISA-BOOLEAN WIP day7 vans order by

--- src/main/java/com/example/vans_back/domain/van/VanRepository.java
+++ src/main/java/com/example/vans_back/domain/van/VanRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface VanRepository extends JpaRepository<Van, Integer> {
-    @Query("select v from Van v where (v.city.id = ?1 or ?1 = 0) and (v.id = ?2 or ?2 = 0) and v.status = ?3")
+    @Query("select v from Van v where (v.city.id = ?1 or ?1 = 0) and (v.id = ?2 or ?2 = 0) and v.status = ?3 order by v.city.name, v.nextInspection")
     List<Van> findVansBy(Integer cityId, Integer vanId, String status);
 
     @Query("select v from Van v where v.status = ?1 order by v.city.name")
     List<Van> findVansBy(String status);
-
-
 
 }
