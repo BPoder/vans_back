794579ba49985eeddc34e154338c26ca15b0d6a0
BPoder <bertpoder@gmail.com>
2023-05-30 17:01:58 +0300

    ISA-BOOLEAN WIP day9 driver add date and km, validate date is available + vanId check + order by

--- src/main/java/com/example/vans_back/domain/van/mileage/MileageRepository.java
+++ src/main/java/com/example/vans_back/domain/van/mileage/MileageRepository.java
@@ -9,11 +9,8 @@ import java.util.List;
 
 public interface MileageRepository extends JpaRepository<Mileage, Integer> {
 
-    @Query("select m from Mileage m where (m.van.id = ?1 or ?1 = 0) and m.date between ?2 and ?3")
+    @Query("select m from Mileage m where (m.van.id = ?1 or ?1 = 0) and m.date between ?2 and ?3 order by m.date, m.van.id")
     List<Mileage> findMileagesBy(Integer vanId, LocalDate startDate, LocalDate endDate);
-
-    @Query("select (count(m) > 0) from Mileage m where m.date = ?1")
-    boolean dateExistsBy(LocalDate date);
 
     @Query("select (count(m) > 0) from Mileage m where m.date = ?1 and m.van.id = ?2")
     boolean dateForVanExistsBy(LocalDate date, Integer id);
